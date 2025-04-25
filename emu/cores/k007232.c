2d2998356b30d6e8a1f178d773eda91646e6bf3e
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-04-25 20:27:11 +0800

    99% done.

--- emu/cores/k007232.c
+++ emu/cores/k007232.c
@@ -1,5 +1,32 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// K007232 core for libvgm, MAME-faithful, C version
+// copyright-holders:Nicola Salmoria,Hiromitsu Shioya
+/*********************************************************/
+/*    Konami PCM controller                              */
+/*********************************************************/
+
+/*
+  Changelog, Hiromitsu Shioya 02/05/2002
+    fixed start address decode timing. (sample loop bug.)
+
+  Changelog, Mish, August 1999:
+    Removed interface support for different memory regions per channel.
+    Removed interface support for differing channel volume.
+
+    Added bankswitching.
+    Added support for multiple chips.
+
+    (NB: Should different memory regions per channel be needed, the bankswitching function can set this up).
+
+  Chanelog, Nicola, August 1999:
+    Added Support for the k007232_VOL() macro.
+    Added external port callback, and functions to set the volume of the channels
+*/
+
+/**********************************************************************************************
+    Konami 007232 PCM controller (Full Working Implementation for libvgm)
+// copyright-holders: Mao, cam900
+***********************************************************************************************/
+
 #include <stdlib.h>
 #include <string.h>
 #include "../../stdtype.h"
@@ -32,12 +59,12 @@ typedef struct {
 	RATIO_CNTR  rateCntr;
 
 	K007232_Channel channel[K007232_PCM_MAX];
-	UINT8 wreg[0x10];
+	UINT8 wreg[0x14];
 	UINT8 *rom;
 	UINT32 rom_size;
 	UINT32 rom_mask;
 
-	UINT8 ext_vol[2];    // [0]=left, [1]=right (external port volume/pan)
+																	   
 	UINT8 loop_en;       // loop control register
 
 	void (*port_write_cb)(UINT8 data); // external port write callback
@@ -85,11 +112,11 @@ static UINT8 device_start_k007232(const DEV_GEN_CFG* cfg, DEV_INFO* retDevInf)
 	chip->rom_mask = 0;
 
 	for (int i = 0; i < K007232_PCM_MAX; i++) {
-		chip->channel[i].vol[0] = (i == 0) ? 255 : 0;
-		chip->channel[i].vol[1] = (i == 1) ? 255 : 0;
+		chip->channel[i].vol[0] = 255;
+		chip->channel[i].vol[1] = 255;
 		chip->channel[i].mute = 0;
 	}
-	chip->ext_vol[0] = 255; chip->ext_vol[1] = 255;
+												
 	chip->port_write_cb = NULL;
 
 	INIT_DEVINF(retDevInf, &chip->_devData, rate, &devDef);
@@ -116,14 +143,16 @@ static void device_reset_k007232(void* chip)
 		ch->counter = 0x1000;
 		ch->step = 0;
 		ch->play = 0;
+		ch->vol[0] = 255;
+		ch->vol[1] = 255;
 	}
+
 	c->channel[0].vol[0] = 255; c->channel[0].vol[1] = 255;
-	c->channel[1].vol[0] = 255;   c->channel[1].vol[1] = 255;
-	c->ext_vol[0] = 255; c->ext_vol[1] = 255;
+	c->channel[1].vol[0] = 255; c->channel[1].vol[1] = 255;
 	RC_RESET(&c->rateCntr);
 }
 
-// --- Streamed update (called from main loop) ---
+// --- Streamed update ---
 static void k007232_update(void* chip, UINT32 samples, DEV_SMPL **outputs)
 {
 	k007232_state* c = (k007232_state*)chip;
@@ -132,8 +161,8 @@ static void k007232_update(void* chip, UINT32 samples, DEV_SMPL **outputs)
 		for (int i = 0; i < K007232_PCM_MAX; i++) {
 			K007232_Channel* ch = &c->channel[i];
 			if (ch->play && c->rom && !ch->mute) {
-				int vol_l = (ch->vol[0] * c->ext_vol[0]) >> 8;
-				int vol_r = (ch->vol[1] * c->ext_vol[1]) >> 8;
+												  
+												  
 				UINT32 pcm_addr = ch->bank + (ch->addr & K007232_ADDR_MASK);
 
 				if (pcm_addr >= c->rom_size)
@@ -142,8 +171,8 @@ static void k007232_update(void* chip, UINT32 samples, DEV_SMPL **outputs)
 				UINT8 value = c->rom[pcm_addr];
 				INT16 out = ((value & 0x7F) - 0x40) << 7;
 
-				lsum += (out * vol_l) >> 7;
-				rsum += (out * vol_r) >> 7;
+				lsum += (out * ch->vol[0]) >> 7;
+				rsum += (out * ch->vol[1]) >> 7;
 
 				ch->counter -= 32;
 				while (ch->counter < 0 && ch->play) {
@@ -173,8 +202,11 @@ static void k007232_update(void* chip, UINT32 samples, DEV_SMPL **outputs)
 
 static void k007232_write(void* chip, UINT8 offset, UINT8 data)
 {
+	if (offset > 0x13)
+		return;
+
 	k007232_state* c = (k007232_state*)chip;
-	c->wreg[offset & 0x0F] = data;
+	c->wreg[offset] = data;
 	int ch = (offset >= 6) ? 1 : 0;
 	int reg_base = ch * 6;
 	K007232_Channel* v = &c->channel[ch];
@@ -187,13 +219,13 @@ static void k007232_write(void* chip, UINT8 offset, UINT8 data)
 		UINT8 pitch_msb = c->wreg[reg_base + 1];
 		UINT8 mode = (pitch_msb >> 4) & 0x03;
 		switch (mode) {
-		case 0x00: // 12-bit frequency mode (normal)
+		case 0x00: // 12-bit frequency mode (normal) (not implemented / dummy)
 			v->step = ((pitch_msb & 0x0F) << 8) | pitch_lsb;
 			break;
-		case 0x01: // 8-bit frequency mode (inverted)
+		case 0x01: // 8-bit frequency mode (inverted) (not implemented / dummy)
 			v->step = (256 - pitch_lsb) << 4;
 			break;
-		case 0x02: // 4-bit frequency mode
+		case 0x02: // 4-bit frequency mode (not implemented / dummy)
 			v->step = (16 - (pitch_msb & 0x0F)) << 8;
 			break;
 		case 0x03: // Reserved/invalid
@@ -208,23 +240,23 @@ static void k007232_write(void* chip, UINT8 offset, UINT8 data)
 	case 0x04: case 0x0A: // Start address MSB (only bit0)
 		v->start = ((c->wreg[reg_base + 4] & 0x01) << 16) | (c->wreg[reg_base + 3] << 8) | c->wreg[reg_base + 2];
 		break;
-	case 0x05: case 0x0B: // Key on
+	case 0x05: case 0x0B: // Key on trigger
 		v->play = 1;
 		v->addr = v->start;
 		v->counter = 0x1000;
 		break;
-	case 0x0C: // External port write (usually volume/pan)
-		c->ext_vol[0] = data; c->ext_vol[1] = data;
+	case 0x0C: // External port write (usually volume/pan nibble) 
+											 
 		if (c->port_write_cb)
 			c->port_write_cb(data);
 		break;
 	case 0x0D: // Loop enable
 		c->loop_en = data;
 		break;
-	case 0x0E: // Bank for channel 0 (upper bits)
+	case 0x0E: // Bankswitch register
 		c->channel[0].bank = data << 17;
 		break;
-	case 0x0F: // Bank for channel 1 (upper bits)
+	case 0x0F: // Bankswitch register
 		c->channel[1].bank = data << 17;
 		break;
 	case 0x10: // Left volume for channel 0
