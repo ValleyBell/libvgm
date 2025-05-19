1809872bc44c2c36b7df65a6a99137d77dea117f
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-05-19 22:36:49 +0800

    Initial MSM5205/MSM6585 implementation.

--- emu/SoundEmu.c
+++ emu/SoundEmu.c
@@ -51,6 +51,7 @@
 #define SNDDEV_GA20
 #define SNDDEV_MIKEY
 #define SNDDEV_K007232
+#define SNDDEV_MSM5205
 #endif
 
 #ifdef SNDDEV_SN76496
@@ -169,6 +170,9 @@
 #endif
 #ifdef SNDDEV_K007232
 #include "cores/k007232.h"
+#endif
+#ifdef SNDDEV_MSM5205
+#include "cores/msm5205.h"
 #endif
 
 const DEV_DECL* sndEmu_Devices[] = {
@@ -300,6 +304,9 @@ const DEV_DECL* sndEmu_Devices[] = {
 #endif
 #ifdef SNDDEV_K007232
 	&sndDev_K007232,
+#endif
+#ifdef SNDDEV_MSM5205
+	&sndDev_MSM5205,
 #endif
 	NULL	// list end
 };
