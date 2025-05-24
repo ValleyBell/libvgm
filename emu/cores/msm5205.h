2c568ee87b4e79effb644e7510359fc951af2063
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-05-24 10:52:08 +0800

    Update msm5205.h from cam900

--- emu/cores/msm5205.h
+++ emu/cores/msm5205.h
@@ -1,9 +1,17 @@
-#ifndef __MSM5205_H__
-#define __MSM5205_H__
-
-#include "../EmuStructs.h"
-
 // Configuration flags (bit 31 for MSM6585)
+#define MSM5205_S1	0
+#define MSM5205_S2	1
+
+#define MSM5205_ADPCM_3B	2
+#define MSM5205_ADPCM_4B	0
+
+typedef struct msm5205_config
+{
+	DEV_GEN_CFG _genCfg;
+
+	UINT8 prescaler;	// prescaler, Bit 0 = S1, Bit 1 = S2
+	UINT8 adpcmBits;	// bits per ADPCM sample (3, 4), 0 = default (3)
+} MSM5205_CFG;
 
 extern const DEV_DECL sndDev_MSM5205;
 
