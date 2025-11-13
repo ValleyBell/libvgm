76eed5dfea79be5200003fb71e9498bb3dc2e6b5
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-11-14 05:46:25 +0800

    Initial MSM5232 implementation for libvgm. (#139)

--- emu/SoundEmu.c
+++ emu/SoundEmu.c
@@ -53,6 +53,7 @@
 #define SNDDEV_K007232
 #define SNDDEV_K005289
 #define SNDDEV_MSM5205
+#define SNDDEV_MSM5232
 #endif
 
 #ifdef SNDDEV_SN76496
@@ -177,6 +178,9 @@
 #endif
 #ifdef SNDDEV_MSM5205
 #include "cores/msm5205.h"
+#endif
+#ifdef SNDDEV_MSM5232
+#include "cores/msm5232.h"
 #endif
 
 const DEV_DECL* sndEmu_Devices[] = {
@@ -314,6 +318,9 @@ const DEV_DECL* sndEmu_Devices[] = {
 #endif
 #ifdef SNDDEV_MSM5205
 	&sndDev_MSM5205,
+#endif
+#ifdef SNDDEV_MSM5232
+	&sndDev_MSM5232,
 #endif
 	NULL	// list end
 };
