7fdc72d225530b7c5730c23f17b3b781cfbda181
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-11-18 21:35:38 +0800

    Adding the excess soundchip.

--- player/vgmplayer.hpp
+++ player/vgmplayer.hpp
@@ -284,8 +284,8 @@ protected:
 	enum
 	{
 		_HDR_BUF_SIZE = 0x100,
-		_OPT_DEV_COUNT = 0x2e,
-		_CHIP_COUNT = 0x2e,
+		_OPT_DEV_COUNT = 0x2f,
+		_CHIP_COUNT = 0x2f,
 		_PCM_BANK_COUNT = 0x40
 	};
 	
